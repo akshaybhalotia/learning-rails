f89807b95e714a77592784acf9a39dad3b53a20a
akshaybhalotia <abhalotia99@gmail.com>
2016-01-13 14:37:56 +0530

    Remove exercise code to display counter

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -8,11 +8,5 @@ class StoreController < ApplicationController
     else
       @products = Product.order(:title)
     end
-    if session[:counter].nil?
-      session[:counter] = 1
-    else
-      session[:counter] += 1
-    end
-    @counter = session[:counter]
   end
 end
